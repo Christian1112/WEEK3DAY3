e64c6d5b89d7a86ca4b05ccf581682421da67bc7
Simon Blaha <s.blaha@live.at>
2019-05-21 14:16:23 +0200

    add getter and setter methods

--- src/Advanced_A4/Book.java
+++ src/Advanced_A4/Book.java
@@ -2,6 +2,7 @@ package Advanced_A4;
 
 public class Book {
 
+    // Enum method
     public enum Status {
         Available, NotAvailable;
     }
@@ -27,5 +28,59 @@ public class Book {
 
     }
 
+    public int getBookID() {
+        return bookID;
+    }
 
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getBookIsbn() {
+        return bookIsbn;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    public String getRentStart() {
+        return rentStart;
+    }
+
+    public String getRentEnd() {
+        return rentEnd;
+    }
+
+    public Status getRentStatus() {
+        return rentStatus;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public void setBookIsbn(String bookIsbn) {
+        this.bookIsbn = bookIsbn;
+    }
+
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public void setRentStart(String rentStart) {
+        this.rentStart = rentStart;
+    }
+
+    public void setRentEnd(String rentEnd) {
+        this.rentEnd = rentEnd;
+    }
+
+    public void setRentStatus(Status rentStatus) {
+        this.rentStatus = rentStatus;
+    }
 }
