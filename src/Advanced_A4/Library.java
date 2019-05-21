3cc4cfe648363a3fde43f44d622e972928725900
Christian Simic <christian.simicibwl@hotmail.com>
2019-05-21 14:02:16 +0200

    Advanced

--- src/Advanced_A4/Library.java
+++ src/Advanced_A4/Library.java
@@ -1,4 +1,16 @@
 package Advanced_A4;
 
 public class Library {
+
+    private String name;
+    private String adress;
+
+    public Library (String name, String adress){
+
+        this.name = name;
+        this.adress = adress;
+
+    }
+
+    
 }
